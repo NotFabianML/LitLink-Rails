722f775e3b8625dc4d90b25aa3b0664fa83aed4f
Fabian <fabianmloaiza@gmail.com>
2025-04-23 18:31:51 -0600

    fix: change login and signup logic

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,35 +2,37 @@ module Api::V1
   class AuthController < ApplicationController
     skip_before_action :authenticate_user!, only: [ :login, :signup, :me ]
 
-    # POST /api/v1/auth/signup
     def signup
-       user = User.new(user_params)
-
+      user = User.new(user_params)
       if user.save
-        token = user.generate_jwt
-        render json: { token: token, user: user }, status: :created
+        render json: {
+          token: user.generate_jwt,
+          user: {
+            id: user.id.to_s,
+            first_name: user.first_name,
+            last_name:  user.last_name,
+            email:      user.email
+          }
+        }, status: :created
       else
-        render json: { errors: user.errors }, status: :unprocessable_entity
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
       end
     end
 
-    # POST /api/v1/auth/login
     def login
-      # Acceder directamente a los parámetros raíz
-      email = params[:email]
-      password = params[:password]
-
-      unless email.present? && password.present?
-        return render json: { error: "Email y contraseña requeridos" }, status: :bad_request
-      end
-
-      user = User.find_by_email(email)
-
-      if user&.authenticate(password)
-        token = user.generate_jwt
-        render json: { token: token, user: user }
+      user = User.find_by_email(params[:email])
+      if user&.authenticate(params[:password])
+        render json: {
+          token: user.generate_jwt,
+          user: {
+            id: user.id.to_s,
+            first_name: user.first_name,
+            last_name:  user.last_name,
+            email:      user.email
+          }
+        }
       else
-        render json: { error: "Invalid credentials" }, status: :unauthorized
+        render json: { error: "Credenciales inválidas" }, status: :unauthorized
       end
     end
 
@@ -42,9 +44,6 @@ module Api::V1
     private
 
     def user_params
-      # params.require(:auth).permit(
-      #   user: [ :first_name, :last_name, :email, :password ]
-      # )[:user]
       params.require(:auth).require(:user).permit(
       :first_name, :last_name, :email, :password, :password_confirmation
       )
