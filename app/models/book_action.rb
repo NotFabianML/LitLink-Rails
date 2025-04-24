79658133b5727e22f91cdf68b7d44b18f2ac3bc2
Fabian <fabianmloaiza@gmail.com>
2025-04-23 18:35:42 -0600

    fix: remove dynamo related code

--- app/models/book_action.rb
+++ app/models/book_action.rb
@@ -13,32 +13,8 @@ class BookAction
 
   SWIPE_ACTIONS = %w[left right].freeze
 
-  # Schema DynamoDB con índices críticos
-  # table name: :book_actions,
-  #       key: :id,
-  #       global_secondary_indexes: [
-  #         {
-  #           hash_key: :user_id,
-  #           range_key: :book_id,
-  #           name: "user_book_index",
-  #           projection: :all
-  #         },
-  #         {
-  #           hash_key: :user_id,
-  #           range_key: :interacted_at,
-  #           name: "user_interaction_timeline",
-  #           projection: :all
-  #         }
-  #       ]
-
-  # Campos
-  # field :user_id, :string
-  # field :book_id, :string
-  # field :status, :integer
-  # field :swipe_action, :string
-  # field :interacted_at, :datetime
-  # field :metadata, :raw
-  field :user_id, type: BSON::ObjectId
+ # Campos
+ field :user_id, type: BSON::ObjectId
   field :book_id, type: BSON::ObjectId
   field :status, type: Integer
   field :swipe_action, type: String
