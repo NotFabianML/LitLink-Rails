459b12afa4442d65df78cf48f632518f02f87e0c
Fabian <fabianmloaiza@gmail.com>
2025-04-23 18:46:14 -0600

    builld: add url to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com", "localhost:3000", "localhost:3001", "localhost:8000"
+    origins "example.com", "localhost:3000", "localhost:3001", "localhost:8000", "litlink-cr.vercel.app"
 
     resource "*",
       headers: :any,
